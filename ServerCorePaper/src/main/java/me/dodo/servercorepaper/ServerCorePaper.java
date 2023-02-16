e61fb012eb0776e5ef80d5350ebd7572f96b1b99
dodoflix <dogukanmetan@gmail.com>
2023-02-16 21:52:28 +0300

    projects added

---
+++ ServerCorePaper/src/main/java/me/dodo/servercorepaper/ServerCorePaper.java
@@ -0,0 +1,17 @@
+package me.dodo.servercorepaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerCorePaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ ServerCoreWaterfall/src/main/java/me/dodo/servercorewaterfall/ServerCoreWaterfall.java
@@ -0,0 +1,16 @@
+package me.dodo.servercorewaterfall;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class ServerCoreWaterfall extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
